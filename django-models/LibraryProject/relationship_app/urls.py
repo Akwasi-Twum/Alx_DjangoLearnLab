428613404912b2de0fbcf1bfe8329185a416a763
Akwasi-Twum <twumk444@gmail.com>
2025-07-26 16:15:53 +0000

    Add files via upload

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views.admin_view import admin_view
-from .views.librarian_view import librarian_view
-from .views.member_view import member_view
+from .views import admin_view, librarian_view, member_view
 
 urlpatterns = [
-    path('admin-view/', admin_view, name='admin_view'),
-    path('librarian-view/', librarian_view, name='librarian_view'),
-    path('member-view/', member_view, name='member_view'),
+    path('admin-role/', admin_view.admin_view, name='admin_view'),
+    path('librarian-role/', librarian_view.librarian_view, name='librarian_view'),
+    path('member-role/', member_view.member_view, name='member_view'),
 ]
