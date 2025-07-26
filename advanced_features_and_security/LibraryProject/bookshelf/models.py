114537186ab7bea06ad3fcf19243b761f89e2d54
Akwasi-Twum <twumk444@gmail.com>
2025-07-26 22:30:41 +0000

    Add files via upload

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -5,8 +5,6 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
-["class CustomUser(AbstractUser):", 
- "date_of_birth", "profile_photo"]
-["class CustomUserManager(BaseUserManager):","create_user", "create_superuser"]
+
     def _ _str_ _(self)
     return f"{self.title} ({self.publication_year})"
