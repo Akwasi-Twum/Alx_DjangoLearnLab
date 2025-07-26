48612342c6289b7ad50210bba797019a761f689e
Akwasi-Twum <twumk444@gmail.com>
2025-07-26 15:19:33 -0700

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -5,6 +5,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
-
+["class CustomUser(AbstractUser):", 
+ "date_of_birth", "profile_photo"]
     def _ _str_ _(self)
     return f"{self.title} ({self.publication_year})"
