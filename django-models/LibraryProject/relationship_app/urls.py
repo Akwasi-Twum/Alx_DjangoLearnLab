e13aca20219499ab69da206039ea66c061be32ab
Akwasi-Twum <twumk444@gmail.com>
2025-07-26 09:55:07 -0700

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin-role/', admin_view.admin_view, name='admin_view'),
     path('librarian-role/', librarian_view.librarian_view, name='librarian_view'),
     path('member-role/', member_view.member_view, name='member_view'),
+    ["add_book/", "edit_book/", "delete_book"]
 ]
