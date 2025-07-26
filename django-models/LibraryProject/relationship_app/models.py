899a366fa02dd3008ed2d1e22a963abe02a3bd31
Akwasi-Twum <twumk444@gmail.com>
2025-07-26 09:41:47 -0700

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -22,4 +22,5 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+    instance.userprofile.save() 
+['"class Meta","permissions"]
