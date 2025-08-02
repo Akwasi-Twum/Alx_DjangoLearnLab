253564e2af198e6ff28f1c87528b15e49daaeb41
Akwasi-Twum <twumk444@gmail.com>
2025-08-02 04:27:30 -0700

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
+["viewsets.ModelViewSet"]
 ["BookViewSet"]
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
