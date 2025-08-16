37b996ee8008b558a9cbabd57e1d86ec3440559c
Akwasi-Twum <twumk444@gmail.com>
2025-08-16 05:18:09 -0700

    Update settings.py

--- advanced-api-project/settings.py
+++ advanced-api-project/settings.py
@@ -1,14 +1,6 @@
 # ... existing imports ...
 INSTALLED_APPS = [
-    # Default Django apps...
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Third-party apps
-    'rest_framework',
+     'rest_framework',
     # Your API app
     'api',
 ]
