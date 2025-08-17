375e48a1d23a5d3d53a4f34023e4998b0c78399e
Akwasi-Twum <twumk444@gmail.com>
2025-08-17 04:21:25 -0700

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("profile/", views.profile_view, name="profile"),
    ["post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/"] 
+    ["comment/<int:pk>/update/", "post/<int:pk>/comments/new/", "comment/<int:pk>/delete/"]
 ]
