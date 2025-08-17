c65a1cabc4aa62a1e2307ecc794c69e645081776
Akwasi-Twum <twumk444@gmail.com>
2025-08-17 03:22:51 -0700

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -58,7 +58,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+ ["USER", "PORT"]
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
